4239b95d4e9cba6260fd9c0a5445b8d503d47712
Rifrer785 <Rifrer@bk.ru>
2020-11-29 18:41:27 +0300

    Добавьте файлы проекта.

---
+++ PPVIS1/PPVIS1.cpp
@@ -0,0 +1,131 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+#include <fstream>
+#include <vocabulary.h>
+using namespace std;
+
+
+
+void Menu()
+{
+	cout << "\tМеню\n"
+		<< "(1)Добавление элемента\n"
+		<< "(2)Вывод данных по элементу\n"
+		<< "(3)Удаление элемента\n"
+		<< "(4)Вывести словарь\n"
+		<< "(5)Размер словаря\n"
+		<< "(6)Изменить значение\n"
+		<< "(7)Сохранить изменения\n"
+		<< "(0)Выйти из программы\n";
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	Vocabulary<string> vocabulary;
+	string Word_E, Word_R;
+	int choose;
+	ofstream fout;
+	ifstream fin;
+	fin.open("vocabulary.txt");
+	if (!fin.is_open())
+	{
+		cout << "Файл не найден. Словарь будет пуст" << endl;
+	}
+	else
+	{
+		getline(fin, Word_E);
+		while (!fin.eof())
+		{
+			getline(fin, Word_E);
+			getline(fin, Word_R);
+			vocabulary.push_back(Word_E, Word_R);
+		}
+		cout << "Файл открыт. Словарь загружен" << endl;
+		system("pause");
+	}
+	fin.close();
+	while (1)
+	{
+		system("cls");
+		Menu();
+		while (1)
+		{
+			cin.clear();
+			cin.ignore(cin.rdbuf()->in_avail());
+			cin >> choose;
+			if (cin.fail())
+			{
+				cin.clear();
+				cin.ignore(30000, '\n');
+				cout << "Некорректное значение, попробуйте еще раз.\n";
+			}
+			else
+			{
+				break;
+			}
+		}
+		switch (choose)
+		{
+		case 1:
+				cout << "Введите английское слово: ";
+				cin.ignore(30000, '\n');
+				getline(cin, Word_E);
+				cout << "\nВведите русское слово: ";
+				getline(cin, Word_R);
+				vocabulary.push_back(Word_E, Word_R);
+				break;
+		case 2:
+			cout << "Введите слово: ";
+			cin.ignore(30000, '\n');
+			getline(cin, Word_E);
+				vocabulary.show_data(Word_E);
+				system("pause");
+				break;
+		case 3:
+			cout << "Введите слово: ";
+			cin.ignore(30000, '\n');
+			getline(cin, Word_E);
+			vocabulary.delete_element(Word_E);
+			system("pause");
+			break;
+		case 4:
+			vocabulary.Show_Tree(vocabulary.head); cout << endl;
+			system("pause");
+			break;
+		case 5:
+			vocabulary.GetSize();
+			system("pause");
+			break;
+		case 6:
+			cout << "Введите английское слово: ";
+			cin.ignore(30000, '\n');
+			getline(cin, Word_E);
+			cout << "\nВведите русское слово для замены: ";
+			getline(cin, Word_R);
+			vocabulary.change_data(Word_E, Word_R);
+			system("pause");
+			break;
+		case 7:
+			fout.open("vocabulary.txt");
+			if (!fout.is_open())
+			{
+				cout << "Ошибка открытия файла" << endl;
+			}
+			else
+			{
+				vocabulary.Fout(vocabulary.head, fout);
+			}
+			fout.close();
+			break;
+		case 0:
+			exit(0);
+			break;
+		default:
+			break;
+		}
+	}
+}
---
+++ PPVIS1/vocabulary.h
@@ -0,0 +1,334 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <Windows.h>
+#include <fstream>
+using namespace std;
+template<typename T>
+class Vocabulary
+{
+private:
+	template<typename T>
+	class Node
+	{
+	public:
+		Node* pRight;
+		Node* pLeft;
+		T Word_E;
+		T Word_R;
+		Node(T Word_E = T(), T Word_R = T(), Node* pRight = nullptr, Node* pLeft = nullptr)
+		{
+			this->Word_E = Word_E;
+			this->Word_R = Word_R;
+			this->pLeft = pLeft;
+			this->pRight = pRight;
+		}
+	};
+public:
+
+	Node<T>* head;
+	int size;
+	Vocabulary();
+	void push_back(T Word_E, T Word_R);
+	void show_data(T Word_E);
+	void change_data(T Word_E, T Word_R);
+	void delete_element(T Word_E);
+	void Show_Tree(Node<T>* Vocabulary);
+	void Fout(Node<T>* current, ofstream& fout);
+	void Paste(Node<T>* Vocabulary);
+	int GetSize()
+	{
+		return size;
+	}
+};
+
+template<typename T>
+Vocabulary<T>::Vocabulary()
+{
+	size = 0;
+	head = nullptr;
+}
+
+template<typename T>
+void Vocabulary<T>::push_back(T Word_E, T Word_R)
+{
+	if (head == nullptr)
+	{
+		head = new Node<T>;
+		head->Word_E = Word_E;
+		head->Word_R = Word_R;
+	}
+	else
+	{
+		Node<T>* current = this->head;
+		while (current->pLeft || current->pRight != nullptr)
+		{
+			if (Word_E <= current->Word_E)
+			{
+				if (current->pLeft == nullptr) { break; }
+				current = current->pLeft;
+			}
+			if (Word_E > current->Word_E)
+			{
+				if (current->pRight == nullptr) { break; }
+				current = current->pRight;
+			}
+		}
+		if (Word_E <= current->Word_E)
+		{
+			current->pLeft = new Node<T>(Word_E);
+			current->pLeft->Word_E = Word_E;
+			current->pLeft->Word_R = Word_R;
+		}
+		else
+		{
+			current->pRight = new Node<T>(Word_E);
+			current->pRight->Word_E = Word_E;
+			current->pRight->Word_R = Word_R;
+		}
+	}
+	size++;
+}
+
+template<typename T>
+void Vocabulary<T>::show_data(T Word_E)
+{
+	Node<T>* current = this->head;
+	if (head == nullptr)
+	{
+		cout << "Словарь пуст\n";
+	}
+	else
+	{
+		while (current != nullptr)
+		{
+			if (Word_E == current->Word_E)
+			{
+				cout << "Английский: " << current->Word_E << "\tРусский: " << current->Word_R << endl;
+				break;
+			}
+			if (current->pLeft == nullptr && current->pRight == nullptr)
+			{
+				current = nullptr;
+				cout << "Такого узла нет!";
+				break;
+			}
+			if (Word_E < current->Word_E && current->pLeft != nullptr)
+			{
+				current = current->pLeft;
+			}
+			if (Word_E > current->Word_E&& current->pRight != nullptr)
+			{
+				current = current->pRight;
+			}
+		}
+	}
+}
+
+template<typename T>
+void Vocabulary<T>::change_data(T Word_E, T Word_R)
+{
+	Node<T>* current = this->head;
+	if (head == nullptr)
+	{
+		cout << "Словарь пуст\n";
+	}
+	else
+	{
+		while (current != nullptr)
+		{
+			if (Word_E == current->Word_E)
+			{
+				current->Word_R = Word_R;
+				cout << "Английский: " << current->Word_E << "\tРусский: " << current->Word_R << endl;
+				break;
+			}
+			if (current->pLeft == nullptr && current->pRight == nullptr)
+			{
+				current = nullptr;
+				cout << "Такого узла нет!";
+				break;
+			}
+			if (Word_E < current->Word_E && current->pLeft != nullptr)
+			{
+				current = current->pLeft;
+			}
+			if (Word_E > current->Word_E&& current->pRight != nullptr)
+			{
+				current = current->pRight;
+			}
+		}
+	}
+}
+
+template<typename T>
+void Vocabulary<T>::Paste(Node<T>* current)//Обратный
+{
+	if (current)
+	{
+		Paste(current->pLeft);
+		Paste(current->pRight);
+		push_back(current->Word_E, current->Word_R);
+	}
+}
+
+template<typename T>
+void Vocabulary<T>::delete_element(T Word_E)
+{
+	Node<T>* current = this->head;
+	Node<T>* save = this->head;
+	Node<T>* savetopush = this->head;
+	if (head == nullptr)
+	{
+		cout << "Словарь пуст\n";
+	}
+	else
+	{
+		while (current->pLeft != nullptr || current->pRight != nullptr)
+		{
+			if (head->Word_E == Word_E)
+			{
+				if (head->pLeft == nullptr && head->pRight == nullptr)
+				{
+					delete(head);
+					head = nullptr;
+					size--;
+					break;
+				}
+				if (head->pLeft != nullptr && head->pRight == nullptr)
+				{
+					save = head->pLeft;
+					delete(head);
+					head = save;
+					size--;
+					break;
+				}
+				if (head->pLeft == nullptr && head->pRight != nullptr)
+				{
+					save = head->pRight;
+					delete(head);
+					head = save;
+					size--;
+					break;
+				}
+				if (head->pLeft != nullptr && head->pRight != nullptr)
+				{
+					savetopush = head->pLeft;
+					save = head->pRight;
+					delete(head);
+					head = save;
+					Paste(savetopush);
+					size--;
+					break;
+				}
+			}
+			if (Word_E == current->pLeft->Word_E)
+			{
+				if (current->pLeft->pLeft == nullptr && current->pLeft->pRight == nullptr)
+				{
+					delete (current->pLeft);
+					current->pLeft = nullptr;
+					size--;
+					break;
+				}
+				if (current->pLeft->pLeft == nullptr && current->pLeft->pRight != nullptr)
+				{
+					save = current->pLeft->pRight;
+					delete (current->pLeft);
+					current = save;
+					size--;
+					break;
+				}
+				if (current->pLeft->pRight == nullptr && current->pLeft->pLeft != nullptr)
+				{
+					save = current->pLeft->pLeft;
+					delete (current->pLeft);
+					current = save;
+					size--;
+					break;
+				}
+				if (current->pLeft->pRight != nullptr && current->pLeft->pLeft != nullptr)
+				{
+					save = current->pLeft->pLeft;
+					savetopush = current->pLeft->pRight;
+					delete (current->pLeft);
+					size--;
+					Paste(savetopush);
+					break;
+				}
+			}
+			if (Word_E == current->pRight->Word_E)
+			{
+				if (current->pRight->pLeft == nullptr && current->pRight->pRight == nullptr)
+				{
+					delete (current->pRight);
+					current->pRight = nullptr;
+					size--;
+					break;
+				}
+				if (current->pRight->pLeft == nullptr && current->pRight->pRight != nullptr)
+				{
+					save = current->pRight->pRight;
+					delete (current->pRight);
+					current = save;
+					size--;
+					break;
+				}
+				if (current->pRight->pRight == nullptr && current->pRight->pLeft != nullptr)
+				{
+					save = current->pRight->pLeft;
+					delete (current->pRight);
+					current = save;
+					size--;
+					break;
+				}
+				if (current->pRight->pRight != nullptr && current->pRight->pLeft != nullptr)
+				{
+					save = current->pRight->pLeft;
+					savetopush = current->pRight->pRight;
+					delete (current->pRight);
+					size--;
+					Paste(savetopush);
+					break;
+				}
+			}
+			if (current->pLeft == nullptr && current->pRight == nullptr)
+			{
+				current = nullptr;
+				cout << "Такого узла нет!";
+				break;
+			}
+			if (Word_E < current->Word_E && current->pLeft != nullptr)
+			{
+				current = current->pLeft;
+			}
+			if (Word_E > current->Word_E&& current->pRight != nullptr)
+			{
+				current = current->pRight;
+			}
+		}
+	}
+}
+
+
+template<typename T>
+void Vocabulary<T>::Show_Tree(Node<T>* current)//Симметричный
+{
+	if (current)
+	{
+		Show_Tree(current->pLeft);
+		cout << current->Word_E << "\t" << current->Word_R << endl;
+		Show_Tree(current->pRight);
+	}
+}
+
+template<typename T>
+void Vocabulary<T>::Fout(Node<T>* current, ofstream& fout)
+{
+	if (current)
+	{
+		Fout(current->pLeft, fout);
+		fout << endl << current->Word_E << endl << current->Word_R;
+		Fout(current->pRight, fout);
+	}
+}
